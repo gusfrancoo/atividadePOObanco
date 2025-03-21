51dfb188ebec473a503590316ff938087f86dce6
gusfrancoo <gustafranco2543@gmail.com>
2025-03-21 19:45:11 -0300

    Atividade resolvida

--- src/main/java/edu/pucgoias/aulasjavafx/HelloController.java
+++ src/main/java/edu/pucgoias/aulasjavafx/HelloController.java
@@ -1,10 +1,12 @@
 package edu.pucgoias.aulasjavafx;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
-public class HelloController {
+public class HelloController implements EventHandler<ActionEvent> {
 
     @FXML
     private Button btnUm;
@@ -16,17 +18,33 @@ public class HelloController {
     private Button btnTres;
 
     @FXML
+    private Label labelMensagem;
+
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
     private void botaoUmClicado(ActionEvent evento) {
-        System.out.println("Evento tratado por um controlador externo!");
+        TratadorEvento trat = new TratadorEvento();
+        trat.setLabel(this.labelMensagem);
+        trat.handle(evento);
     }
 
     @FXML
     private void botaoDoisClicado(ActionEvent evento) {
-        System.out.println("Evento tratado via expressão Lambda!");
+        this.labelMensagem.setText("Ação botão 2");
     }
 
     @FXML
     private void botaoTresClicado(ActionEvent evento) {
-        System.out.println("Evento tratado na própria classe controladora!");
+        this.handleButton();
     }
+
+    private void handleButton(){
+        this.labelMensagem.setText("Botão tratado por método externo");
+    }
+
+
 }
--- src/main/java/edu/pucgoias/aulasjavafx/TratadorEvento.java
+++ src/main/java/edu/pucgoias/aulasjavafx/TratadorEvento.java
@@ -2,10 +2,19 @@ package edu.pucgoias.aulasjavafx;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class TratadorEvento implements EventHandler<ActionEvent> { // 1
 
-    public void handle(ActionEvent evento) { // 2
-        System.out.println("Evento tratado por uma classe externa");
+    private Label labelMensagem;
+
+    public void setLabel(Label labelMensagem) {
+        this.labelMensagem = labelMensagem;
+    }
+
+    @Override
+    public void handle(ActionEvent evento) {
+            labelMensagem.setText("Clique tratado na superclasse" );
     }
 }
